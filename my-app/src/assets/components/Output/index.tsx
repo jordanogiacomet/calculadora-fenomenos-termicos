b1754767502fc674bed604c9c8448cd39f0538d2
Jordano <JGTomazoni@ucs.br>
2022-12-13 23:22:00 -0300

    Calculadora Finalizada

--- my-app/src/assets/components/Output/index.tsx
+++ my-app/src/assets/components/Output/index.tsx
@@ -2,21 +2,35 @@ import { useEffect, useRef, useState } from "react";
 import Botao from "../Botao";
 import GrausParaKelvin from "../Calculadora/GrausParaKelvin";
 import style from '../Output/Output.module.scss'
+import GrausParaFanthet from "../Calculadora/GrausParaFanheith";
 
 export default function Output(props:{name:string, temperatura:number}){
-    const [state, setState] = useState(0)
+    const [kelvin, setKelvin] = useState(0)
+    const [fahrenheit, setFahrenheit] = useState(0)
+
+    //setKelvin(tempKelvin.calcularTemperatura(Number(e.target.value)))}
 
 
-    let oldState = useRef(state);
+    let tempKelvin = new GrausParaKelvin
 
-    let temp = new GrausParaKelvin
+    let tempFanheith = new GrausParaFanthet
+
+
+    
+
+
 
 
 
     return(
         <div className={style.output}>
-             <input type="number" onChange={e => setState(temp.calcularTemperatura(Number(e.target.value)))} required pattern="[0-9]*[.,][0-9]*" />
-             <h4>{state}</h4>
+             <input type="number" onChange={(e) => {setKelvin(tempKelvin.calcularTemperatura(Number(e.target.value))); setFahrenheit(tempFanheith.calcularTemperatura(Number(e.target.value)))}} required pattern="[0-9]*[.,][0-9]*" />
+              
+
+              <p>Temperatura em Kelvin</p>
+             <h4>{kelvin}</h4>
+             <p>Temperatura em Fahrenheit</p>
+             <h4>{fahrenheit}</h4>
         </div>
         
         )
